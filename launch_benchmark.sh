cbd542c2b26218e98e3fe2e4df088761f20dcb40
zengxian <xiangdong.zeng@intel.com>
2023-05-08 10:00:49 +0800

    for ResNeSt

---
+++ launch_benchmark.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+set -xe
+
+# resnest50
+function main {
+    # prepare workload
+    workload_dir="${PWD}"
+    # set common info
+    source oob-common/common.sh
+    init_params $@
+    fetch_device_info
+    set_environment
+
+    pip install -r ${workload_dir}/requirements.txt
+    # pip install --no-deps torchvision -f https://download.pytorch.org/whl/torch_stable.html
+    if [ ! -d zhanghang1989_ResNeSt_master ];then
+        git clone https://github.com/zhanghang1989/ResNeSt.git zhanghang1989_ResNeSt_master
+        cd zhanghang1989_ResNeSt_master
+        git checkout 1dfb3e8867e2ece1c28a65c9db1cded2818a2031
+        cd ..
+        cache_dir="${HOME}/.cache/torch/hub"
+        mkdir -p ${cache_dir} && rm -rf ${cache_dir}/zhanghang1989_ResNeSt_master
+        cp -r zhanghang1989_ResNeSt_master ${cache_dir}
+    fi
+
+    # if multiple use 'xxx,xxx,xxx'
+    model_name_list=($(echo "${model_name}" |sed 's/,/ /g'))
+    batch_size_list=($(echo "${batch_size}" |sed 's/,/ /g'))
+
+    # generate benchmark
+    for model_name in ${model_name_list[@]}
+    do
+        # pre run
+        python scripts/torch/verify.py --model resnest50 --batch-size 1 --no-cuda \
+            --warmup 1 --max_iters 2 --dummy --workers 1 \
+            --precision ${precision} --channels_last ${channels_last} || true
+        #
+        for batch_size in ${batch_size_list[@]}
+        do
+            # clean workspace
+            logs_path_clean
+            # generate launch script for multiple instance
+            if [ "${OOB_USE_LAUNCHER}" == "1" ] && [ "${device}" != "cuda" ];then
+                generate_core_launcher
+            else
+                generate_core
+            fi
+            # launch
+            echo -e "\n\n\n\n Running..."
+            cat ${excute_cmd_file} |column -t > ${excute_cmd_file}.tmp
+            mv ${excute_cmd_file}.tmp ${excute_cmd_file}
+            source ${excute_cmd_file}
+            echo -e "Finished.\n\n\n\n"
+            # collect launch result
+            collect_perf_logs
+        done
+    done
+}
+
+function generate_core {
+    # generate multiple instance script
+    for(( i=0; i<instance; i++ ))
+    do
+        real_cores_per_instance=$(echo ${cpu_array[i]} |awk -F, '{print NF}')
+        log_file="${log_dir}/rcpi${real_cores_per_instance}-ins${i}.log"
+
+        # instances
+        if [ "${device}" != "cuda" ];then
+            OOB_EXEC_HEADER=" numactl -m $(echo ${device_array[i]} |awk -F ';' '{print $2}') "
+            OOB_EXEC_HEADER+=" -C $(echo ${device_array[i]} |awk -F ';' '{print $1}') "
+        else
+            OOB_EXEC_HEADER=" CUDA_VISIBLE_DEVICES=${device_array[i]} "
+        fi
+
+        printf " ${OOB_EXEC_HEADER} \
+            python scripts/torch/verify.py --model resnest50 --no-cuda --dummy --workers 1 \
+                --warmup ${num_warmup} --max_iters ${num_iter}  \
+                --batch-size ${batch_size} \
+                --precision ${precision} \
+                --channels_last ${channels_last} \
+                ${addtion_options} \
+        > ${log_file} 2>&1 &  \n" |tee -a ${excute_cmd_file}
+        if [ "${numa_nodes_use}" == "0" ];then
+            break
+        fi
+    done
+    echo -e "\n wait" >> ${excute_cmd_file}
+}
+
+
+# run
+function generate_core_launcher {
+    # generate multiple instance script
+    for(( i=0; i<instance; i++ ))
+    do
+        real_cores_per_instance=$(echo ${cpu_array[i]} |awk -F, '{print NF}')
+        log_file="${log_dir}/rcpi${real_cores_per_instance}-ins${i}.log"
+
+        printf "python -m oob-common.launch --enable_jemalloc \
+                    --core_list $(echo ${device_array[@]} |sed 's/;.//g') \
+                    --log_file_prefix rcpi${real_cores_per_instance} \
+                    --log_path ${log_dir} \
+                    --ninstances ${#cpu_array[@]} \
+                    --ncore_per_instance ${real_cores_per_instance} \
+            scripts/torch/verify.py --model resnest50 --no-cuda --dummy --workers 1 \
+                --warmup ${num_warmup} --max_iters ${num_iter}  \
+                --batch-size ${batch_size} \
+                --precision ${precision} \
+                --channels_last ${channels_last} \
+                ${addtion_options} \
+        > /dev/null 2>&1 &  \n" |tee -a ${excute_cmd_file}
+        break
+    done
+    echo -e "\n wait" >> ${excute_cmd_file}
+}
+
+# download common files
+rm -rf oob-common && git clone https://github.com/intel-sandbox/oob-common.git
+
+# Start
+main "$@"
--- scripts/torch/verify.py
+++ scripts/torch/verify.py
@@ -11,6 +11,7 @@ from __future__ import print_function
 import os
 import argparse
 from tqdm import tqdm
+import time
 
 import torch
 import torch.nn as nn
@@ -48,6 +49,20 @@ class Options():
                             help='put the path to resuming file if needed')
         parser.add_argument('--verify', type=str, default=None,
                             help='put the path to resuming file if needed')
+        parser.add_argument('--ipex', action='store_true', default=False,
+                    help='use ipex')
+        parser.add_argument('--jit', action='store_true', default=False,
+                            help='use ipex')
+        parser.add_argument('--precision', default="float32",
+                                help='precision, "float32" or "bfloat16"')
+        parser.add_argument('--warmup', type=int, default=10,
+                            help='number of warmup')
+        parser.add_argument('--max_iters', type=int, default=500,
+                            help='max number of iterations to run')
+        parser.add_argument('--dummy', action='store_true', default=False,
+                            help='use dummy data')
+        parser.add_argument('--channels_last', type=int, default=1, help='use channels last format')
+        parser.add_argument('--profile', action='store_true', help='Trigger profile on current topology.')
         self.parser = parser
 
     def parse(self):
@@ -55,34 +70,59 @@ class Options():
         return args
 
 
+def save_profile_result(filename, table):
+    import xlsxwriter
+    workbook = xlsxwriter.Workbook(filename)
+    worksheet = workbook.add_worksheet()
+    keys = ["Name", "Self CPU total %", "Self CPU total", "CPU total %" , "CPU total", \
+            "CPU time avg", "Number of Calls"]
+    for j in range(len(keys)):
+        worksheet.write(0, j, keys[j])
+
+    lines = table.split("\n")
+    for i in range(3, len(lines)-4):
+        words = lines[i].split(" ")
+        j = 0
+        for word in words:
+            if not word == "":
+                worksheet.write(i-2, j, word)
+                j += 1
+    workbook.close()
+
+
 def main():
     # init the args
     args = Options().parse()
+
+    if args.ipex:
+        import intel_extension_for_pytorch as ipex
+        print("Running with IPEX...")
+
     args.cuda = not args.no_cuda and torch.cuda.is_available()
-    print(args)
     torch.manual_seed(args.seed)
     if args.cuda:
         torch.cuda.manual_seed(args.seed)
-    # init dataloader
-    interp = PIL.Image.BILINEAR if args.crop_size < 320 else PIL.Image.BICUBIC
-    base_size = args.base_size if args.base_size is not None else int(1.0 * args.crop_size / 0.875)
-    transform_val = transforms.Compose([
-        ECenterCrop(args.crop_size),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                             std=[0.229, 0.224, 0.225]),
-    ])
-    valset = ImageNetDataset(transform=transform_val, train=False)
-    val_loader = torch.utils.data.DataLoader(
-        valset, batch_size=args.batch_size, shuffle=False,
-        num_workers=args.workers, pin_memory=True if args.cuda else False)
+    if not args.dummy:
+        # init dataloader
+        interp = PIL.Image.BILINEAR if args.crop_size < 320 else PIL.Image.BICUBIC
+        base_size = args.base_size if args.base_size is not None else int(1.0 * args.crop_size / 0.875)
+        transform_val = transforms.Compose([
+            ECenterCrop(args.crop_size),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225]),
+        ])
+        valset = ImageNetDataset(transform=transform_val, train=False)
+        val_loader = torch.utils.data.DataLoader(
+            valset, batch_size=args.batch_size, shuffle=False,
+            num_workers=args.workers, pin_memory=True if args.cuda else False)
     
     # init the model
     model_kwargs = {}
 
-    assert args.model in torch.hub.list('zhanghang1989/ResNeSt', force_reload=True)
-    model = torch.hub.load('zhanghang1989/ResNeSt', args.model, pretrained=True)
-    print(model)
+    assert args.model in torch.hub.list('zhanghang1989/ResNeSt', force_reload=False)
+    model = torch.hub.load('zhanghang1989/ResNeSt', args.model, pretrained=False)
+    # print(model)
 
     if args.cuda:
         model.cuda()
@@ -107,22 +147,152 @@ def main():
                 format(args.resume))
 
     model.eval()
+
+    if args.channels_last:
+        model_oob = model
+        try:
+            model_oob.to(memory_format=torch.channels_last)
+            print("Use NHWC model.")
+        except:
+            print("Use normal model.")
+        if args.jit:
+            data = torch.randn(args.batch_size, 3, args.crop_size, args.crop_size)
+            model_oob = torch.jit.trace(model_oob, data.to(memory_format=torch.channels_last))
+        model = model_oob
+
+    if args.ipex:
+        if args.precision == "bfloat16":
+            model = ipex.optimize(model, dtype=torch.bfloat16, inplace=True)
+            print('Running with bfloat16...')
+        else:
+            model = ipex.optimize(model, dtype=torch.float32, inplace=True)
+            print('Running with float32...')
+
+    if args.jit:
+        data = torch.randn(args.batch_size, 3, args.crop_size, args.crop_size)
+        model = torch.jit.trace(model, data)
+        if args.ipex:
+            model = torch.jit.freeze(model)
+        #warmup
+        for i in range(10):
+            model(data)
+
     top1 = AverageMeter()
     top5 = AverageMeter()
+    batch_time = AverageMeter()
+    batch_time_list = []
     is_best = False
-    tbar = tqdm(val_loader, desc='\r')
-    for batch_idx, (data, target) in enumerate(tbar):
-        if args.cuda:
-            data, target = data.cuda(), target.cuda()
-        with torch.no_grad():
-            output = model(data)
-            acc1, acc5 = accuracy(output, target, topk=(1, 5))
-            top1.update(acc1[0], data.size(0))
-            top5.update(acc5[0], data.size(0))
+    if args.dummy:
+        max_iters = args.max_iters + args.warmup
+        if args.precision == "bfloat16":
+            with torch.cpu.amp.autocast(enabled=True, dtype=torch.bfloat16):
+                for batch_idx in range(max_iters):
+                    data = torch.randn(args.batch_size, 3, args.crop_size, args.crop_size)
+                    target = torch.arange(1, args.batch_size + 1).long()
+                    if args.cuda:
+                        data, target = data.cuda(), target.cuda()
+                    if args.channels_last:
+                        try:
+                            data, target = data.to(memory_format=torch.channels_last), target.to(memory_format=torch.channels_last)
+                        except:
+                            pass
+                    start = time.time()
+                    with torch.no_grad():
+                        if args.profile:
+                            with torch.profiler.profile(activities=[torch.profiler.ProfilerActivity.CPU], record_shapes=True) as prof:
+                                output = model(data)
+                        else:
+                            output = model(data)
+                    end = time.time()
+                    print("Iteration: {}, inference time: {} sec.".format(batch_idx, end - start), flush=True)
+                    if batch_idx >= args.warmup:
+                        batch_time.update(end - start)
+                        batch_time_list.append((end - start) * 1000)
 
-        tbar.set_description('Top1: %.3f | Top5: %.3f'%(top1.avg, top5.avg))
+                    if batch_idx % 10 == 0:
+                        print('iters: {:d}/{:d}, {:0.3f}({:0.3f}).'.format(batch_idx + 1, max_iters, batch_time.val, batch_time.avg))
 
-    print('Top1 Acc: %.3f | Top5 Acc: %.3f '%(top1.avg, top5.avg))
+                    if batch_idx >= max_iters -1:
+                        break
+        else:
+            for batch_idx in range(max_iters):
+                data = torch.randn(args.batch_size, 3, args.crop_size, args.crop_size)
+                target = torch.arange(1, args.batch_size + 1).long()
+                if args.cuda:
+                    data, target = data.cuda(), target.cuda()
+                if args.channels_last:
+                    try:
+                        data, target = data.to(memory_format=torch.channels_last), target.to(memory_format=torch.channels_last)
+                    except:
+                        pass
+                start = time.time()
+                with torch.no_grad():
+                    if args.profile:
+                        with torch.profiler.profile(activities=[torch.profiler.ProfilerActivity.CPU], record_shapes=True) as prof:
+                            output = model(data)
+                    else:
+                        output = model(data)
+                end = time.time()
+                print("Iteration: {}, inference time: {} sec.".format(batch_idx, end - start), flush=True)
+                if batch_idx >= args.warmup:
+                    batch_time.update(end - start)
+                    batch_time_list.append((end - start) * 1000)
+
+                if batch_idx % 10 == 0:
+                    print('iters: {:d}/{:d}, {:0.3f}({:0.3f}).'.format(batch_idx + 1, max_iters, batch_time.val, batch_time.avg))
+
+                if batch_idx >= max_iters -1:
+                    break
+    else:
+        tbar = tqdm(val_loader, desc='\r')
+        max_iters = min(args.max_iters + args.warmup, len(tbar)) if args.max_iters > 0 else len(tbar)
+        for batch_idx, (data, target) in enumerate(tbar):
+            if batch_idx >= args.warmup:
+                start = time.time()
+            elif args.cuda:
+                data, target = data.cuda(), target.cuda()
+
+            with torch.no_grad():
+                output = model(data)
+                acc1, acc5 = accuracy(output, target, topk=(1, 5))
+                top1.update(acc1[0], data.size(0))
+                top5.update(acc5[0], data.size(0))
+            if batch_idx >= args.warmup:
+                batch_time.update(time.time() - start)
+                batch_time_list.append((time.time() - start) * 1000)
+
+            tbar.set_description('Top1: %.3f | Top5: %.3f'%(top1.avg, top5.avg))
+
+            if batch_idx >= max_iters -1:
+                break
+
+        print('Top1 Acc: %.3f | Top5 Acc: %.3f '%(top1.avg, top5.avg))
+    latency = batch_time.avg / args.batch_size * 1000
+    throughput = args.batch_size / batch_time.avg
+    print("\n", "-"*20, "Summary", "-"*20)
+    print("inference latency:\t {:.3f} ms".format(latency))
+    print("inference Throughput:\t {:.2f} samples/s".format(throughput))
+    # P50
+    batch_time_list.sort()
+    p50_latency = batch_time_list[int(len(batch_time_list) * 0.50) - 1]
+    p90_latency = batch_time_list[int(len(batch_time_list) * 0.90) - 1]
+    p99_latency = batch_time_list[int(len(batch_time_list) * 0.99) - 1]
+    print('Latency P50:\t %.3f ms\nLatency P90:\t %.3f ms\nLatency P99:\t %.3f ms\n'\
+            % (p50_latency, p90_latency, p99_latency))
+
+    #
+    if args.profile:
+        import pathlib
+        timeline_dir = str(pathlib.Path.cwd()) + '/timeline/'
+        if not os.path.exists(timeline_dir):
+            os.makedirs(timeline_dir)
+        timeline_file = timeline_dir + 'timeline-' + str(torch.backends.quantized.engine) + '-' + \
+                    "resnest" + str(batch_idx) + '-' + str(os.getpid()) + '.json'
+        print(timeline_file)
+        prof.export_chrome_trace(timeline_file)
+        table_res = prof.key_averages().table(sort_by="cpu_time_total")
+        print(table_res)
+        # self.save_profile_result(timeline_dir + torch.backends.quantized.engine + "_result_average.xlsx", table_res)
 
 class ECenterCrop:
     """Crop the given PIL Image and resize it to desired size.
@@ -179,12 +349,12 @@ class AverageMeter(object):
         self.reset()
 
     def reset(self):
-        #self.val = 0
+        self.val = 0
         self.sum = 0
         self.count = 0
 
     def update(self, val, n=1):
-        #self.val = val
+        self.val = val
         self.sum += val * n
         self.count += n
 
